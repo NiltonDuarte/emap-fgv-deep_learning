262ced07371df71076ba140312a6f6144a341e6f
Nilton Duarte <nilton.gduarte@gmail.com>
2021-01-09 21:17:09 -0300

    fixing var scope

--- COVID/script/data_processing.py
+++ COVID/script/data_processing.py
@@ -6,12 +6,12 @@ destiny_folder = '../dataset/preprocessed'
 
 
 def process_NORMAL_images():
-    raw_folder = f'{raw_folder}/NORMAL'
-    destiny_folder = f'{destiny_folder}/NORMAL'
-    _, _, images = next(walk(raw_folder))
+    _raw_folder = f'{raw_folder}/NORMAL'
+    _destiny_folder = f'{destiny_folder}/NORMAL'
+    _, _, images = next(walk(_raw_folder))
     for image in images:
-        image_path = f"{raw_folder}/{image}"
-        destiny_path = f"{destiny_folder}/{image}"
+        image_path = f"{_raw_folder}/{image}"
+        destiny_path = f"{_destiny_folder}/{image}"
         print(f'processing image {image}')
         im = Image.open(image_path)
         region = im.crop((100, 100, 1024, 1024))
@@ -21,16 +21,17 @@ def process_NORMAL_images():
 
 
 def process_COVID_images():
-    raw_folder = f'{raw_folder}/COVID'
-    destiny_folder = f'{destiny_folder}/COVID'
-    _, _, images = next(walk(raw_folder))
+    _raw_folder = f'{raw_folder}/COVID'
+    _destiny_folder = f'{destiny_folder}/COVID'
+    _, _, images = next(walk(_raw_folder))
     for image in images:
-        image_path = f"{raw_folder}/{image}"
-        destiny_path = f"{destiny_folder}/{image}"
+        image_path = f"{_raw_folder}/{image}"
+        destiny_path = f"{_destiny_folder}/{image}"
         print(f'processing image {image}')
         im = Image.open(image_path)
         grayscale = im.convert('L')
-        grayscale.save(destiny_path)
+        resized = grayscale.resize((256, 256), Image.LANCZOS)
+        resized.save(destiny_path)
 
 
 def upload_data():
@@ -51,6 +52,6 @@ def upload_data():
 
 
 if __name__ == '__main__':
-    # process_NORMAL_images()
-    # process_COVID_images()
-    upload_data()
+    process_NORMAL_images()
+    process_COVID_images()
+    # upload_data()
